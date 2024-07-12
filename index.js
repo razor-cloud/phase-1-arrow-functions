9ce16f92cee852ab1b5fba45082e8a5fa5982556
trevor <nicholastrevor05@gmail.com>
2024-07-12 23:04:55 +0300

    Complete

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression: divide
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function: square
+  const square = x => x * x;
+  
+  // Arrow function: add
+  const add = (a, b) => a + b;
